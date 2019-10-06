cd4770f0e40e5ac5ef6dc8bfbef8e3706c438885
foxcatcher24 <tmitul515@gmail.com>
2019-10-06 18:37:33 +0600

    Seventh commit: Updated Shopping list and recipe list.

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,11 @@
+export class Ingredient {
+  // public name: string;
+  // public amount: number;
+
+  /* the following property will behave similar to the prior method. */
+  constructor( public name: string, public amount: number ) {
+  // constructor( name: string, amount: number ) {
+    // this.name = name;
+    // this.amount = amount;
+  }
+}
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+
+  ingredients: Ingredient[] = [
+    new Ingredient('avocado', 4),
+    new Ingredient('chicken', 1),
+    new Ingredient('tomatoes', 3)
+  ];
 
   constructor() { }
 
