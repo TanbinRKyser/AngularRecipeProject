39bb7f6a85e3ef4d0fc3d551b1a63f20b20a0658
foxcatcher24 <tmitul515@gmail.com>
2019-11-02 19:57:07 +0600

    Adding ingredients to Recipes.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,16 +1,29 @@
 import { Recipe } from './recipe.modle';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
 
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('Perfect Guacamole', 'Sample',
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Guacomole.jpg/1920px-Guacomole.jpg'),
-    new Recipe('Chicken Tacos', 'Sample',
-      // tslint:disable-next-line: max-line-length
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/001_Tacos_de_carnitas%2C_carne_asada_y_al_pastor.jpg/1280px-001_Tacos_de_carnitas%2C_carne_asada_y_al_pastor.jpg')
+    new Recipe( 'Perfect Guacamole',
+                'Sample',
+                'https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Guacomole.jpg/1920px-Guacomole.jpg',
+                [
+                  new Ingredient('Meat',1),
+                  new Ingredient('French Fires',20)
+                ]
+              ),
+    new Recipe( 'Chicken Tacos',
+                'Sample',
+                // tslint:disable-next-line: max-line-length
+                'https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/001_Tacos_de_carnitas%2C_carne_asada_y_al_pastor.jpg/1280px-001_Tacos_de_carnitas%2C_carne_asada_y_al_pastor.jpg'
+                , [
+                  new Ingredient('Chicken', 2),
+                  new Ingredient('Sauce', 2)
+                ]
+              )
   ];
 
   getRecipes() {
